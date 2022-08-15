825bd0453a025c3d288dccfb5617ec6d92d85305
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-08-15 22:26:53 +0700

    add modules/forms

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,5 @@ import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    forms();
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,5 +1,5 @@
 const forms = () => {
-    const form = document.querySelectorAll('form'),
+    const forms = document.querySelectorAll('form'),
         inputs = document.querySelectorAll('input');
     
     const message = {
@@ -8,7 +8,61 @@ const forms = () => {
         successText: 'Наш менеджер свяжется с вами в ближайшее время!',
         failure: 'Что-то пошло не так...'
     };
-    
+
+    forms.forEach(item => {
+        bindPostData(item);
+    });
+
+    const postData = async(url, data) => {
+        const res  = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+            postData('./server.php', json)
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal_mini');
+        thanksModal.innerHTML = `
+        <div class="modal__content">
+            <div class="modal__close" data-close>×</div>
+            <div class="modal__title">${message}</div>
+        </div>
+        `;
+        document.querySelector('.overlay').append(thanksModal);
+
+    }
+
 
 };
 
